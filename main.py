b3e54578aaabc3d5f4d0d0a4671ac8c8c0c2623f
Henry <henryjhli98@gmail.com>
2024-03-14 20:40:42 -0400

    debugging

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ cred = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scop
 
 client = gspread.authorize(cred)
 
-sheet = client.open('Birthday_list')
+sheet = client.open("test")
 
 worksheet = sheet.get_worksheet(0)
 
