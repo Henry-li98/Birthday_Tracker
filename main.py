752fbe9d974a7f84b1c8031f8f1b77c13f9f93b7
Henry <henryjhli98@gmail.com>
2024-04-11 22:13:47 -0400

    building out bubble sort

--- main.py
+++ main.py
@@ -61,9 +61,16 @@ while True:
 
 print("sorting months by alphabetical order")
 
-def bubblesort(df.index):
+def bubblesort():
     n = len(df.index)
     for i in range(n-1):
+        swapped = False
+        for j in range(0, n-1-i):
+            if arr[j] > arr[j+1]
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+            if swapped == False:
+                break
 
 
 # learn to use bubble sort for this
