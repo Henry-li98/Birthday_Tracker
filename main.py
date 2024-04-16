e4c831879ccdc763a7e154dd32931d2175d4a29a
Henry <henryjhli98@gmail.com>
2024-04-16 17:29:35 -0400

    fixing syntax, testing sort

--- main.py
+++ main.py
@@ -63,10 +63,11 @@ print("sorting months by alphabetical order")
 
 def bubblesort():
     n = len(df.index)
+    print(n)
     for i in range(n-1):
         swapped = False
         for j in range(0, n-1-i):
-            if arr[j] > arr[j+1]
+            if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
                 swapped = True
             if swapped == False:
