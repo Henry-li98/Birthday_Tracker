50c84c5ba5364fcc522b9bc64b4a9d89a1cf19ff
Henry <henryjhli98@gmail.com>
2024-05-08 18:29:59 -0400

    writing in comments of next steps to think about

--- main.py
+++ main.py
@@ -56,6 +56,10 @@ worksheet.update([df_sorted.columns.values.tolist()] + df_sorted.values.tolist()
 now = datetime.now()
 current_date = now.strftime("%m/%d")
 print(current_date)
+
+# with current date and list of organized dates obtained, determine a method on how to look at each entry and determine the next birthday and give a number of days until it occurs
+for i in df_sorted:
+
 # look into how would you might delete or search for a specific person within the list or give everyone within this month
 
 #look into key value pairs and dictionaries
