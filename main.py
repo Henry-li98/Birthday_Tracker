d113996359bd3b3069072add87f6585ec65d05c2
Henry <henryjhli98@gmail.com>
2024-05-29 16:30:02 -0400

    testing how to get the comparison to work

--- main.py
+++ main.py
@@ -60,11 +60,11 @@ current_date = now.strftime("%m/%d")
 print(current_date)
 print(df_sorted.Birthday)
 # with current date and list of organized dates obtained, determine a method on how to look at each entry and determine the next birthday and give a number of days until it occurs
-for i in df_sorted:
-    if current_date < df_sorted.Birthday:
-        i += 1
-    elif current_date >= df_sorted:
-        print("next birthday is " + name + " in these days")
+print("this is the start of the test loop")
+for i in df_sorted.Birthday:
+    # print(i)
+    if current_date >= i:
+        print(f"next birthday is {i=}")
         break
 
 
